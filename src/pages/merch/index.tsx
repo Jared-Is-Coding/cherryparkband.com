adbf207f7a3866c84ab635aaf6fba9b3ef136136
Jared Harrison <jared@jarediscoding.com>
2024-05-10 00:59:55 -0400

    Revert DB info

--- src/pages/merch/index.tsx
+++ src/pages/merch/index.tsx
@@ -8,7 +8,6 @@ import MetaData from "../../components/MetaData"
 import Navbar from "../../components/Navbar"
 import OrdersInformation from "../../components/OrdersInformation"
 import "../../scss/merch.scss"
-import { allMerch } from "./allMerch"
 
 export const Head: HeadFC = () => (
     <>
@@ -16,6 +15,40 @@ export const Head: HeadFC = () => (
         <MetaData title="Merch | Cherry Park" />
     </>
 )
+
+const allMerch: merchData[] = [
+    {
+        title: "Cherry Park Tee",
+        price: "$22.00 - $28.00",
+        imageSrc: "/images/merch/tee_collage.jpg",
+        url: "https://square.link/u/p7Xu6VAp?src=embed"
+    },
+    {
+        title: "Cherry Park Long Sleeve",
+        price: "$32.00 - $38.00",
+        imageSrc: "/images/merch/long_sleeve_collage.jpg",
+        url: "https://square.link/u/KCjEsfoc?src=embed"
+    },
+    {
+        title: "Cherry Park Tote Bag",
+        price: "$10.00",
+        imageSrc: "/images/merch/tote.jpg",
+        url: "https://square.link/u/RfAXVbM0?src=embed"
+    },
+    {
+        title: "Cherry Park Beanie",
+        price: "$25.00",
+        imageSrc: "/images/merch/beanie.jpg",
+        url: "https://square.link/u/BcqKDEn0?src=embed"
+    },
+    /* {
+        dateAvailable: "February 16, 2024",
+        title: "Cherry Park Hoodie",
+        price: "$42.00 - $48.00",
+        imageSrc: "",
+        url: "https://square.link/u/zvcgoIlW?src=embed"
+    } */
+]
 
 const mapMerch = (item: merchData, index: number) => (
     <>
--- src/pages/shows.tsx
+++ src/pages/shows.tsx
@@ -2,11 +2,10 @@ import { type HeadFC, type PageProps } from "gatsby"
 import * as React from "react"
 import { Col, Row } from "react-bootstrap"
 import Container from 'react-bootstrap/Container'
-import Footer from "../../components/Footer"
-import MetaData from "../../components/MetaData"
-import Navbar from "../../components/Navbar"
-import ShowCard from "../../components/ShowCard"
-import { allShows } from "./allShows"
+import Footer from "../components/Footer"
+import MetaData from "../components/MetaData"
+import Navbar from "../components/Navbar"
+import ShowCard from "../components/ShowCard"
 
 export const Head: HeadFC = () => (
     <>
@@ -14,6 +13,59 @@ export const Head: HeadFC = () => (
         <MetaData title="Shows | Cherry Park" />
     </>
 )
+
+const allShows: showData[] = [
+    {
+        showTitle: "TBA",
+        showDate: "August 17, 2024"
+    },
+    {
+        showTitle: "TBA",
+        showDate: "June 22, 2024"
+    },
+    {
+        showTitle: "The Queen, Wilmington DE",
+        showDate: "April 5, 2024",
+        otherPerformers: "The Carraways | Avery Hirsch",
+        imageSrc: "/images/venues/the_queen.png",
+        imageTitle: "The Queen venue image",
+        imageAlt: "Venue image for The Queen in Wilmington Delaware",
+        url: "https://thequeenwilmington.com/",
+        target: "_blank"
+    },
+    {
+        showTitle: "Elkton Music Hall, Elkton MD",
+        showDate: "February 17, 2024",
+        otherPerformers: "Syd Lynrowe",
+        imageSrc: "/images/venues/elkton_music_hall.png",
+        imageTitle: "Elkton Music Hall venue image",
+        imageAlt: "Venue image for Elkton Music Hall in Elkton Maryland",
+        url: "https://www.elktonmusichall.com/",
+        target: "_blank",
+        ageRequirement: true
+    },
+    {
+        showTitle: "Elkton Music Hall, Elkton MD",
+        showDate: "December 2, 2023",
+        otherPerformers: "Kendra Morris",
+        imageSrc: "/images/venues/elkton_music_hall.png",
+        imageTitle: "Elkton Music Hall venue image",
+        imageAlt: "Venue image for Elkton Music Hall in Elkton Maryland",
+        url: "https://www.elktonmusichall.com/",
+        target: "_blank",
+        ageRequirement: true
+    },
+    {
+        showTitle: "The Queen, Wilmington DE",
+        showDate: "September 24, 2023",
+        otherPerformers: "Chvnce | Rachel Ana Dobken",
+        imageSrc: "/images/venues/the_queen.png",
+        imageTitle: "The Queen venue image",
+        imageAlt: "Venue image for The Queen in Wilmington Delaware",
+        url: "https://thequeenwilmington.com/",
+        target: "_blank"
+    }
+]
 
 const today = new Date().valueOf()
 const dateStringValue = (d: string) => new Date(d + " 23:59:59").valueOf()
